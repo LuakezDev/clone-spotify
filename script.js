add29c85e527cc5535e81001f8dfa23dd8912601
LuakezDev <lucianoae93@gmail.com>
2024-01-30 18:44:13 -0300

    Criando api local e utilizando o DOM

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const searchInput = document.getElementById('search-input');
 const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
 
+/*?name_like=${searchTerm}*/
 function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
-    fetch(url)
-        .then((response) => response.json())
-        .then((result) => displayResults(result))
-}
+    fetch(`http://localhost:3000/artists`)
+      .then((response) => response.json())
+      .then((results) => displayResults(results));
+  }
 
 function displayResults(result) {
     resultPlaylist.classList.add("hidden")
@@ -27,8 +27,9 @@ document.addEventListener('input', function () {
     if (searchTerm === '') {
         resultPlaylist.classList.add('hidden');
         resultArtist.classList.remove('hidden');
-        return
+        return;
     }
     
     requestApi(searchTerm);
-})
+});
+
